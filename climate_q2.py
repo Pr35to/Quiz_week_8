4828562c85932172184dde557324f63f9a8c5ea8
Liam Preston <133867495+Pr35to@users.noreply.github.com>
2023-09-11 11:52:00 +0200

    Update climate_q2.py

--- climate_q2.py
+++ climate_q2.py
@@ -1,9 +1,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import csv
 
 years = []
 co2 = []
 temp = []
+
+with open('climate.csv', 'r') as file:
+  reader = csv.reader(file)
+  next(reader)
+  for row in reader:
+    years.append(int(row[0]))
+    co2.append(float(row[1]))
+    temp.append(float(row[2]))
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
